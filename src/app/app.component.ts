70a03c7ad5e74dd70761c8bc5223fa80511f4428
ledemkam <ledemkam@gmail.com>
2025-05-19 19:17:01 +0200

    install and implement Prettier and Eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Hello, Angular intro !';
