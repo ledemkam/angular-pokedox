1f81681659bbc96eb6b16d4170a43761d6ee010b
ledemkam <ledemkam@gmail.com>
2025-05-22 11:05:54 +0200

    implemnt datepipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { POKEMON_LIST } from './pokemon-list.fake';
 import { Pokemon } from './pokemon.model';
 import { PokemonBorderDirective } from './pokemon-border.directive';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [PokemonBorderDirective],
+  imports: [PokemonBorderDirective,DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
