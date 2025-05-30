dcffba7e543d78a7f3c99ff14b92aa09f40eea5c
ledemkam <ledemkam@gmail.com>
2025-05-30 23:06:48 +0200

    implmnt page not found

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 import { PokemonProfilComponent } from './pokemon/pokemon-profil/pokemon-profil.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes : Routes = [
   { path: 'pokemons', component: PokemonListComponent  },
   {path: 'pokemons/:id', component: PokemonProfilComponent  },
   {path: '', redirectTo: '/pokemons', pathMatch: 'full'  },
+  {path: '**', component: PageNotFoundComponent },
 ]
 
 export const appConfig: ApplicationConfig = {
