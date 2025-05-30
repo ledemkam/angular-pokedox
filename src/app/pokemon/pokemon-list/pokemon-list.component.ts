e752cb44d58d7bc339815bcb5583ddbdad765268
ledemkam <ledemkam@gmail.com>
2025-05-30 22:25:05 +0200

    RouterLink implnt

--- src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -4,10 +4,11 @@ import { PokemonService } from '../../pokemon.service';
 import { Pokemon } from '../../pokemon.model';
 import { PokemonBorderDirective } from '../../pokemon-border.directive';
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
-  imports: [PokemonBorderDirective, DatePipe],
+  imports: [PokemonBorderDirective, DatePipe,RouterLink],
   templateUrl: './pokemon-list.component.html',
   styles: ``
 })
