de53eef38a42b06030482506012a99d3adaa35ab
ledemkam <ledemkam@gmail.com>
2025-05-30 22:10:59 +0200

    navigation system install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 
 
@@ -6,7 +7,7 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
