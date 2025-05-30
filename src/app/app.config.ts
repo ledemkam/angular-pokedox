ba331d31591ec5b07eab85effa410cb9b9fc7c33
ledemkam <ledemkam@gmail.com>
2025-05-30 23:09:02 +0200

    implmnt page not found update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,8 @@ import { PokemonProfilComponent } from './pokemon/pokemon-profil/pokemon-profil.
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes : Routes = [
-  { path: 'pokemons', component: PokemonListComponent  },
   {path: 'pokemons/:id', component: PokemonProfilComponent  },
+  { path: 'pokemons', component: PokemonListComponent  },
   {path: '', redirectTo: '/pokemons', pathMatch: 'full'  },
   {path: '**', component: PageNotFoundComponent },
 ]
