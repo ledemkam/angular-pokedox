dcd093c05900fac8785cd0303cec2b062037183a
ledemkam <ledemkam@gmail.com>
2025-05-30 22:16:25 +0200

    pokemon profil component implemnt

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, Routes } from '@angular/router';
 import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
+import { PokemonProfilComponent } from './pokemon/pokemon-profil/pokemon-profil.component';
 
 const routes : Routes = [
   { path: 'pokemons', component: PokemonListComponent  },
+  {path: 'pokemons/:id', component: PokemonProfilComponent  },
   {path: '', redirectTo: '/pokemons', pathMatch: 'full'  },
 ]
 
---
+++ src/app/pokemon/pokemon-profil/pokemon-profil.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-profil',
+  imports: [],
+  templateUrl: './pokemon-profil.component.html',
+  styles: ``
+})
+export class PokemonProfilComponent {
+
+}
